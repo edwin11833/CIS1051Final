5ab36c383b1be2a65c7d5b69ccd31f61a7a4560f
edwin11833 <109628983+edwin11833@users.noreply.github.com>
2023-12-11 00:41:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,87 @@
 #The URL above is the github repo provided by Google Maps for Developers, its a python repo that has the needed libraries.
 #googlemaps library is installed. Check to see if I need anything else??? Maybe Beautiful soup??
 
-import random
 import googlemaps
+import pandas as pd
+import random
 
-api_key = ""
+def miles_to_meters(miles):
+    try:
+        return miles * 1609.34
+    except:
+        return 0
 
-user_zipcode = int(input("Please enter your zipcode: "))
+def lat_long_from_zipcode(zipcode, gmaps_client):
+    try:
+        geocode_result = gmaps_client.geocode(zipcode)
+        if geocode_result and len(geocode_result) > 0:
+            lat = geocode_result[0]["geometry"]["location"]["lat"]
+            lng = geocode_result[0]["geometry"]["location"]["lng"]
+            return lat, lng
+        else:
+            return None, None
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return None, None
 
 
-rest_list = ['applebees','fridays','chipotle','mcdonalds', 'olive garden','red lobster','five guys'] 
-#eventually this has to be a dictionary for filters
+api_key = open('api key.txt', 'r').read()
 
-def rand_choice():
-  pick = random.choice(rest_list)
-  return pick
 
-if user_zipcode in googlemaps:
-  googlemaps
+gmaps = googlemaps.Client(api_key)
+
+
+user_zipcode = input("Please enter your zipcode: ")
+type_of_culture_filter = input("What type of culture is the food from? i.e(American, Mexican, Chinese, etc) ")
+
+
+latitude, longitude = lat_long_from_zipcode(user_zipcode, gmaps)
+user_location = (latitude, longitude)
+search_words = 'restaurants ' + type_of_culture_filter
+user_radius = miles_to_meters(10)
+restaurant_list = []
+
+gmaps_response = gmaps.places_nearby(
+    location=user_location,
+    keyword=search_words,
+    radius=user_radius
+)
+
+if 'results' in gmaps_response:
+    for place in gmaps_response['results']:
+        name = place.get('name')
+        address = place.get('vicinity')
+        rating = place.get('rating', 'No rating') 
+        place_id = place.get('place_id')
+        restaurant_lati = place['geometry']['location']['lat']
+        restaurant_long = place['geometry']['location']['lng']
+
+        restaurant = {
+            'name': name,
+            'address': address,
+            'rating': rating,
+            'latitude': restaurant_lati, 
+            'longitude': restaurant_long, 
+            'place_id': place_id
+        }
+        restaurant_list.append(restaurant)
+
+    if restaurant_list:
+        random_restaurant = random.choice(restaurant_list)
+        print("Here's a random restaurant you might like:")
+        print(f"Name: {random_restaurant['name']}")
+        print(f"Address: {random_restaurant['address']}")
+        print(f"Rating: {random_restaurant['rating']}")
+        
+    else:
+        print("No restaurants found with the given filters.")
+
+else:
+    print("No results found.")
+
+
+
+if latitude is not None and longitude is not None:
+    print(f"Latitude: {latitude}, Longitude: {longitude}")
+else:
+    print("Could not find longitude and latitude for the zipcode.")
